2b4e4a1ef0482419a5d2eac3e4c91d96046c235a
Matt Golub <mgolub@stanford.edu>
2018-10-14 14:33:08 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name = 'recurrent-whisperer',
     version = '1.0.0',
-    url = 'https://github.com/mattgolub/recurrent-whisperer.git',
+    url = 'https://github.com/mattgolub/recurrent-whisperer',
     author = 'Matt Golub',
     author_email = 'mgolub@stanford.edu',
     description = 'A general class template for training recurrent neural networks using Tensorflow',
     packages = find_packages(),
     install_requires = ['numpy >= 1.15.2', 'scipy >= 1.1.0', 'pyyaml >= 3.13'],
+    license='Apache 2.0',
 )
