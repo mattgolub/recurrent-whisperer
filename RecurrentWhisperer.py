2c03eaf0e94bd3fe0813e2afa0d5b473b6e3801f
Matt-linux-workstation <mgolub@stanford.edu>
2021-11-12 18:47:26 -0800

    Fixed is_done check in _do_save_pred()

--- RecurrentWhisperer.py
+++ RecurrentWhisperer.py
@@ -4219,7 +4219,7 @@ class RecurrentWhisperer(object):
 
         Returns: bool indicating whether or not to perform the save.
         '''
-        if self.is_done:
+        if self.self.is_done(self.run_dir):
 
             if version == 'seso':
                 return False
